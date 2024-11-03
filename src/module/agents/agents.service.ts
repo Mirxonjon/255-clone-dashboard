c30f396105401fc924579e9bdef8f476948a219f
Mirxonjon <mirxonjonismanov152@gmail.com>
2024-11-03 22:01:23 +0500

    fixed bug

--- src/module/agents/agents.service.ts
+++ src/module/agents/agents.service.ts
@@ -1238,8 +1238,8 @@ try {
     const theCurrentHour = atDate.getHours();
     const theCurrentMinut = atDate.getMinutes();
     const RequestTimeMinutes = [0, 10, 20];
-    console.log(theCurrentHour , theCurrentMinut);
-    const as = await ControlAgentGraphNB('20-08', theCurrentHour, this.#_cache);
+    // console.log(theCurrentHour , theCurrentMinut);
+    // const as = await ControlAgentGraphNB('20-08', theCurrentHour, this.#_cache);
 
     if (RequestTimeMinutes.includes(theCurrentMinut)) {
       if (theCurrentHour == 7) {
